b4d5f81376263d6ce87d3ad0b0ce9dde3bd87212
Audri Rian <154531143+Audri-Rian@users.noreply.github.com>
2024-09-22 21:21:37 -0300

    Add files via upload

---
+++ JavaFX-MoneyWisdom/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.javafx to javafx.fxml;
+    exports com.example.javafx;
+}
