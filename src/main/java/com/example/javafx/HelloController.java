ee67f3898458d7405f4981db122a9a1fb0e2d8f7
Audri-Rian <audririan1@gmail.com>
2024-09-27 11:04:01 -0300

    Alteração numero 2

--- src/main/java/com/example/javafx/HelloController.java
+++ src/main/java/com/example/javafx/HelloController.java
@@ -4,11 +4,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.event.ActionEvent;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class HelloController {
@@ -17,21 +18,53 @@ public class HelloController {
     private Button btnSubmit;
 
     @FXML
+    private Button btnSubmit2;
+
+    @FXML
+    private Button btnSubmit3;
+
+    @FXML
+    private Button btnSubmit4;
+
+    @FXML
+    private Button btnSubmit5;
+
+    @FXML
+    private Button btnSubmit6;
+
+    @FXML
+    private Button btnSubmit7;
+
+    @FXML
     public void handleSubmitAction(ActionEvent event) {
 
+        Button clickedButton = (Button) event.getSource();
+        clickedButton.setStyle("-fx-background-color: #14469C;");
 
-        try {
-            Parent secondPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("transacoesBancarias.fxml")));
 
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        String fxmlFile = "";
+        if (clickedButton == btnSubmit) {
+            fxmlFile = "transacoesBancarias.fxml";
+        } else if (clickedButton == btnSubmit2) {
+            fxmlFile = "vendas.fxml";
+        } else if (clickedButton == btnSubmit3) {
+            fxmlFile = "fluxoDeCaixa.fxml";
+        } else if (clickedButton == btnSubmit4){
+            fxmlFile = "despesas.fxml";
+        } else if (clickedButton == btnSubmit5){
+            fxmlFile = "funcionarios.fxml";
+        } else if(clickedButton == btnSubmit6){
+            fxmlFile = "Relatorios.fxml";
+        }
 
-            // Define a nova cena
-            Scene scene = new Scene(secondPage);
-            stage.setScene(scene);
-            stage.show();
-            btnSubmit.setStyle("-fx-background-color: #14469C;");
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (!fxmlFile.isEmpty()) {
+            try {
+                Pane novaPagina = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(fxmlFile)));
+                Stage stage = (Stage) clickedButton.getScene().getWindow();
+                stage.setScene(new Scene(novaPagina));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
